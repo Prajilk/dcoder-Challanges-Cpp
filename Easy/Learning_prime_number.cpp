ced116a0324e80d8a08ea0fb4b05fe8f4294fb90
Prajilk <76465233+Prajilk@users.noreply.github.com>
2021-02-06 13:24:23 +0530

    Create Learning_prime_number.cpp

---
+++ Easy/Learning_prime_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i, j, x, y;
+  
+  
+  cin>>x>>y;
+  
+  for(i=x; i<=y; i++)
+  {
+    for(j=2; j<=i; j++)
+    {
+      if(i%j==0)
+      {
+        break;
+      }
+    }
+    if(i==j)
+    {
+      cout<<i<<endl;
+    }
+  }
+  
+}
